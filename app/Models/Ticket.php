1abf421adb3741b0fdeb656d569949474468a24c
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2024-09-28 22:43:48 -0500

    campos para agregar en tickect

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'attendee_id',
+        'ticket_type',
+        'price',
+        'discount_code',
+        'purchase_date'
+    ];
 }
